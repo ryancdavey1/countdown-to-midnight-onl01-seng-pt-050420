249f34fae0b39339e3c4a92ba0eb9be79a2398ab
Ryan D <ryancdavey1@gmail.com>
2020-05-07 17:05:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
     number -= 1
     
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -15,5 +15,5 @@ def countdown_with_sleep(number)
     number -= 1
     sleep 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end  
